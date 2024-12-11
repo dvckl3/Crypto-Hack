30ada22f86a3fe9761175fbbdcd9d1289c6ccf20
Le Tri Duc <letriduc.dn@gmail.com>
2024-12-11 08:47:12 +0700

    Create Gram-Schmidt.py

---
+++ Lattices/Gram-Schmidt.py
@@ -0,0 +1,19 @@
+from sage.all import *
+v1 = vector([4, 1, 3, -1])
+v2 = vector([2, 1, -3, 4])
+v3 = vector([1, 0, -2, 7])
+v4 = vector([6, 2, 9, -5])
+
+def gram_schmidt(vectors):
+    orthogonal_basic=[]
+    for v in vectors:
+      u=v
+      for w in orthogonal_basic:
+        u=u-(v.dot_product(w)/w.dot_product(w))*w
+      orthogonal_basic.append(u)
+    return orthogonal_basic
+
+
+vectors=[v1,v2,v3,v4]
+orthogonal_basic=gram_schmidt(vectors)
+print(orthogonal_basic)
