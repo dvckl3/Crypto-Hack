d7861d2225af83745aec3d1abb4a7f95b806c921
Le Tri Duc <letriduc.dn@gmail.com>
2024-12-11 08:47:47 +0700

    Update Gram-Schmidt.py

--- Lattices/Gram-Schmidt.py
+++ Lattices/Gram-Schmidt.py
@@ -17,3 +17,15 @@ def gram_schmidt(vectors):
 vectors=[v1,v2,v3,v4]
 orthogonal_basic=gram_schmidt(vectors)
 print(orthogonal_basic)
+
+
+# hoặc trong sage cũng có tích hợp các hàm để tính cơ sở trực giao 
+from sage.all import *
+v1 = vector([4, 1, 3, -1])
+v2 = vector([2, 1, -3, 4])
+v3 = vector([1, 0, -2, 7])
+v4 = vector([6, 2, 9, -5])
+vectors=[v1,v2,v3,v4]
+orthogonal_vectors, _ = matrix(vectors).gram_schmidt()
+for vec in orthogonal_vectors:
+    print(vec)
